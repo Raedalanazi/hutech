b565d65bb40009b176d2dae5c6949153f8815bf7
Raedalanazi <rms.993@hotmail.com>
2025-05-31 14:38:44 +0300

    Update employee.py

--- backend/models/employee.py
+++ backend/models/employee.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel
+from typing import Optional
 
+class Employee(BaseModel):
+    id: int
+    name: str
+    position: str
+    department: Optional[str] = None
+    email: Optional[str] = None
